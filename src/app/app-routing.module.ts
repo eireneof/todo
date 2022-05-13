5d775a6d03b60f42e48e53c3cb4507527b1dd73c
eireneof <eof@ic.ufal.br>
2022-05-13 16:53:54 -0300

    feat: criando rota para new-task

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NewTaskComponent } from './components/new-task/new-task.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'new-task',
+    component: NewTaskComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
