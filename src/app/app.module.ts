1bd5480aaf57493ba1880e33c392f0f42aadd964
eireneof <eof@ic.ufal.br>
2022-05-13 16:20:19 -0300

    feat: criando componente de new-task

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NewTaskComponent } from './components/new-task/new-task.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewTaskComponent
   ],
   imports: [
     BrowserModule,
