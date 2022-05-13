e21625e29311e19c02695fd736e8e10d8b634a7f
eireneof <eof@ic.ufal.br>
2022-05-13 16:55:49 -0300

    feat: criando form group e form control para controle do formulário de novas tarefas

--- src/app/components/new-task/new-task.component.ts
+++ src/app/components/new-task/new-task.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/Task';
+import { FormControl, FormGroup } from '@angular/forms';
+import { Guid } from 'guid-typescript';
+import { LocalStorageService } from 'src/app/services/local-storage.service';
 
 @Component({
   selector: 'app-new-task',
@@ -7,9 +11,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewTaskComponent implements OnInit {
 
-  constructor() { }
+  tasks: Task[] = [];
+  newTaskForm: any;
+
+  constructor(private localStorageService: LocalStorageService) { }
 
   ngOnInit(): void {
+    this.tasks = [];
+    this.newTaskForm = new FormGroup({
+      taskId: new FormControl(),
+      name: new FormControl(),
+      isCompleted: new FormControl()
+    });
+  }
+
+  setNewTask(): void {
+    this.newTaskForm.value.taskId = Guid.create().toString();
+    this.newTaskForm.value.isCompleted = false;
+    const newTask: Task = this.newTaskForm.value;
+    this.tasks.push(newTask);
+    this.localStorageService.set('BD', this.tasks);
+    this.newTaskForm.reset();
+
   }
 
 }
